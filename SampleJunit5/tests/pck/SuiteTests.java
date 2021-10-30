406322871293e845a5de83f610c092f0f498c060
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2021-10-30 15:20:53 +0300

    SuiteTests.java

---
+++ SampleJunit5/tests/pck/SuiteTests.java
@@ -0,0 +1,14 @@
+package pck;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({ModuleATests.class,ModuleBTests.class})
+class SuiteTests {
+
+}
